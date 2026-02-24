8ada58d2df4bb9f7d832a6febe6bf2402b9ae5b1
Usman <uz2@njit.edu>
2026-02-23 21:46:39 -0500

    Change header to system report

--- Lab_4/monitor.sh
+++ Lab_4/monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-echo "Memory Log - $(date)" >> system_log.txt
+echo "SYSTEM REPORT - $(Memory)" >> system_log.txt
 free -h | grep Mem >> system_log.txt
 echo "--------------------------------" >> system_log.txt
